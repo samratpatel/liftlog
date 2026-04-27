44d44aa5894a52ed5622a5f4a86150b31270e774
samratpatel <samrat.patel@gmail.com>
2026-04-26 21:57:49 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'liftlog-v1';
-const ASSETS = ['./lift_log.html', './manifest.json'];
+const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
